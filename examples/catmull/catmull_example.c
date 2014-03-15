cc5ee7675b51aa8cb992a6313a0410f473f69bef
tejonbiker <tejonbiker@gmail.com>
2014-03-14 23:02:57 -0600

    the catmull example was extended

--- examples/catmull/catmull_example.c
+++ examples/catmull/catmull_example.c
@@ -22,6 +22,16 @@ varShaders VarShaders;
 BaseShader 	ShaderInfo;
 WEShaderVars    points_shader_vars;
 
+typedef struct tagBackgroud{
+	GLint  texture;
+	float  points[3*4];
+	float  color[4*4];
+	float  texcoord[2*4];
+	GLuint indices[6];
+}Background;
+
+Background BackgroundImage;
+
 int win_width, win_height;
 float proj_matrix[16];
 
@@ -203,7 +213,7 @@ void window_resize()
 
 }
 
-void setup()
+int setup(int argc, char **argv)
 {
 	int i;
 	srand(time(NULL));
@@ -244,14 +254,37 @@ void setup()
 		arrayCPoints[i].color[1]=1.0-(N_COLORPOINTS-i)/(float)N_COLORPOINTS;
 		arrayCPoints[i].color[2]=1.0-(N_COLORPOINTS-i)/(float)N_COLORPOINTS;
 	}
+
+
+	printf("Loading texture: %s ... ",argv[1]);
+	fflush(stdout);
+	//Load background image
+        BackgroundImage.texture=SOIL_load_OGL_texture(argv[1],SOIL_LOAD_AUTO,SOIL_CREATE_NEW_ID,
+				      SOIL_FLAG_MIPMAPS |SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT);
+
+	if(BackgroundImage.texture==0)
+	{
+			printf("Error in SOIL loading image %s, SOIL says: %s\n",argv[1],SOIL_last_result() );
+			return -1;
+	}
+
+	printf("ok\n");
+	printf("Textures loaded!!\n");
+
 }
 
-int main(void)
+int main(int argc, int **argv)
 {
 	WEVect3 *ptrData=NULL;
 	int nPoints;
 	int res=0;
 	int i;
+
+	if(argc!=2)
+	{
+		printf("Usage: catmull_example.bin test_image.jpg\n");
+		return -1;
+	}
 
 	printf("Init EGL...\n");
 	res =glpiInit();
@@ -284,7 +317,10 @@ int main(void)
 	}
 
 	
-	setup();
+	res=setup(argc,argv);
+
+	if(res<0)
+		return -1;
 
 	points_shader_vars.vertex_attrib= VarShaders.Position;
 	points_shader_vars.color_attrib = VarShaders.Colors;
